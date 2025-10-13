14a5bd821d147c9ff2cd2cb08ef2b42215c81ebe
Aftab S <90192423+aftab-s@users.noreply.github.com>
2025-10-13 23:54:11 +0530

    Update AWS profile in provider configuration

--- S3/provider.tf
+++ S3/provider.tf
@@ -11,7 +11,7 @@ terraform {
 
 provider "aws" {
   region  = "us-east-1"
-  profile = "008971661427_AdministratorAccess"
+  profile = "315293714260_DevOps-Engineer-I"
 
   default_tags {
     tags = {
