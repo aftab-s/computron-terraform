846edf3ef754e7094642da6cdde6b781371e3ca7
Aftab S <aftab.mehrab@gmail.com>
2025-10-23 08:27:36 +0530

    update: minor fixes

--- Local File/local.tf
+++
@@ -1,9 +0,0 @@
-resource "local_file" "devops" {
-    filename = var.file_name
-    content = "Terraform is awesome!"
-}
-
-resource "local_file" "cloud" {
-    filename = "cloud.txt"
-    content = "AWS is great!"
-}
---
+++ Local File/main.tf
@@ -0,0 +1,8 @@
+# resource "local_file" "devops" {
+#     filename = var.file_name
+#     content = "Terraform is awesome!"
+# }
+resource "local_file" "cloud" {
+    filename = var.file_name
+    content = "AWS is great!"
+}
